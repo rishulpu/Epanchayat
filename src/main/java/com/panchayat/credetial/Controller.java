6fad4493b9f16d5d7029d8f4d1a5ba6c21855f56
Rishu Kumar <rishu.kumar2024@lpu.in>
2026-03-03 16:49:11 +0530

    register

--- src/main/java/com/panchayat/credetial/Controller.java
+++ src/main/java/com/panchayat/credetial/Controller.java
@@ -20,7 +20,7 @@ public class Controller {
 		return ResponseEntity.ok("Ok");
 	}
 	
-	@PostMapping("/registr")
+	@PostMapping("/register")
 	public ResponseEntity<String> regist(@RequestParam String ss){
 		return ResponseEntity.ok("Ok");
 	}
