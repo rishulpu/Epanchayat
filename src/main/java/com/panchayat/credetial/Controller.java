d3c290861e31705a7bec583ba9354c6e4606927f
Rishu Kumar <rishu.kumar2024@lpu.in>
2026-03-03 16:53:45 +0530

    get module add

--- src/main/java/com/panchayat/credetial/Controller.java
+++ src/main/java/com/panchayat/credetial/Controller.java
@@ -1,11 +1,13 @@
 package com.panchayat.credetial;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
 
 import com.panchayat.LoginDTO;
 
@@ -23,6 +25,11 @@ public class Controller {
 	@PostMapping("/register")
 	public ResponseEntity<String> regist(@RequestParam String ss){
 		return ResponseEntity.ok("Ok");
+	}
+	
+	@GetMapping("/get")
+	public String getuserdata() {
+		return "data found";
 	}
 	
 	
