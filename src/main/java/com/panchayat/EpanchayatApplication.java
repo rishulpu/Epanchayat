22ad18d86bc6837097ce8b6adac83e18e93625bc
RISHU KUMAR <rishu.kumar2024@lpu.in>
2026-03-04 21:20:29 +0530

    Update EpanchayatApplication.java
    Isko edit kiye h

--- src/main/java/com/panchayat/EpanchayatApplication.java
+++ src/main/java/com/panchayat/EpanchayatApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EpanchayatApplication {
+//Edited by rishu
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpanchayatApplication.class, args);
